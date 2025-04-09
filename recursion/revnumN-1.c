9ee5386990a1206b8ee50baa5334d72701798373
Adyasha Nanda <adyashananda105@gmail.com>
2025-04-09 22:12:51 +0530

    added to rec folder

---
+++ recursion/revnumN-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void f(int i,int n){
+    if(i<1){
+        return;
+    }
+    else{
+        printf("%d\n",i);
+        f(i-1,n);   
+    }
+}
+
+int main(){
+    int n,i;
+    f(10,10);
+}
