dbd4965fb9995199b8720874d73060c9fd9bf719
Adyasha Nanda <adyashananda105@gmail.com>
2025-04-30 11:10:53 +0530

    bubble sort

---
+++ sorting/bubblesort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100], n, i, j;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Sorting the array using bubble sort
+    for (i = n-1; i >=1; i--)
+    {
+        for (j = 0; j <=i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // Swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("The sorted array is: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
