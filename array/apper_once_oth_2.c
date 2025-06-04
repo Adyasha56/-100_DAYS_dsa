45a21f1e9d1e30c9b976f033f171749931c05f04
Adyasha Nanda <adyashananda105@gmail.com>
2025-06-04 11:44:24 +0530

    optimal sol for finding that no inside a array whicj apparer once and others are apperas twice

---
+++ array/apper_once_oth_2.c
@@ -0,0 +1,28 @@
+//find the no in the array which occurs once and others twice
+
+//brute will be linear search and do thye itearion update  the counter 
+//better will be using hash and map data structure
+//optimal will be using bit manipulation i.e xor operation
+
+#include <stdio.h>
+int main()
+{
+    int arr[100],n,i,xor=0;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d",&n);
+
+    printf("Enter the elements of the array:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    
+    }
+
+    for(i=0;i<n;i++)
+    {
+        xor ^= arr[i]; // XOR operation
+    }
+
+    printf("The number that occurs once is: %d\n", xor);
+    return 0;
+}
