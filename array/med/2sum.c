6139094a6b08fa5530d279407aa1d37990b5e137
Adyasha Nanda <adyashananda105@gmail.com>
2025-06-06 10:01:08 +0530

    2 sum prob variety 1

---
+++ array/med/2sum.c
@@ -0,0 +1,41 @@
+//2 sum problem, variety-1 , only yes or no 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[1000],n,i,j,sum;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the sum to be checked: ");
+    scanf("%d", &sum);
+
+    int hashmap[1000] = {0}; // Assuming the sum and elements are within this range
+    for(i = 0; i < n; i++)
+    {
+        int complement = sum - arr[i];
+        if(complement >= 0 && complement < 1000 && hashmap[complement] > 0)
+        {
+            //indices of those two ele 
+
+            printf("Yes\n");
+            printf("Indices: %d and %d\n", i, hashmap[complement]);
+            printf("Elements: %d and %d\n", arr[i], complement);
+            return 0;
+        }
+        
+        hashmap[arr[i]]++;
+    }
+    printf("No\n");
+    return 0;
+
+}
